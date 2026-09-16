906f35e6e9a07c2c9f64b67812d5d149a18441b0
cw06725-debug <cw06725@gmail.com>
2026-09-16 12:36:12 +0500

    CellTracker v0.9.3.0.67 fix Kotlin compile error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.celltracker"
         minSdk = 29
         targetSdk = 34
-        versionCode = 140
-        versionName = "0.9.3.0.66"
+        versionCode = 141
+        versionName = "0.9.3.0.67"
     }
 
     buildFeatures {
